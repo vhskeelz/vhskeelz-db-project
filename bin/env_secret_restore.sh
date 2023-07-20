28036deae82ff4f8d2ba78fcaa20ddd760ac7d92
Ori Hoch <ori@uumpa.com>
2023-07-20 18:57:30 +0300

    add sync of secrets

--- bin/env_secret_restore.sh
+++ bin/env_secret_restore.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo Restoring .env from secrets manager... &&\
-cp .env .env.bak &&\
+if [ -f .env ]; then cp .env .env.bak; fi &&\
 gcloud --project skeelz-retrain-api secrets versions access \
   "$(gcloud --project skeelz-retrain-api secrets versions list vhskeelz-db-env --filter="state=enabled" --format="value(name)" | head -n1)" \
   --secret=vhskeelz-db-env \
