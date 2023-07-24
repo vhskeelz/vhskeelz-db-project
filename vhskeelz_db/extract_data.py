cbebcce336207a3afebda61b6cbe91180c989143
Ori Hoch <ori@uumpa.com>
2023-07-24 10:45:30 +0300

    add retries on gspread api exceptions

--- vhskeelz_db/extract_data.py
+++ vhskeelz_db/extract_data.py
@@ -3,8 +3,23 @@ import csv
 
 from . import config
 
+import backoff
 import gspread
 from google.oauth2.service_account import Credentials as ServiceAccountCredentials
+
+
+@backoff.on_exception(backoff.expo, gspread.exceptions.APIError, max_time=60*30)
+def list_spreadsheets(gc):
+    return gc.list_spreadsheet_files()
+
+
+@backoff.on_exception(backoff.expo, gspread.exceptions.APIError, max_time=60*30)
+def get_all_sheet_values(gc, spreadsheet, sheet_name=None):
+    if sheet_name:
+        sheet = gc.open(spreadsheet['name']).worksheet(sheet_name)
+    else:
+        sheet = gc.open(spreadsheet['name']).sheet1
+    return sheet.get_all_values()
 
 
 def main(log, only_table_name=None):
@@ -16,7 +31,7 @@ def main(log, only_table_name=None):
     )
     log('Fetching matching sheets...')
     matching_tables = {}
-    spreadsheets = gc.list_spreadsheet_files()
+    spreadsheets = list_spreadsheets(gc)
     spreadsheets.sort(key=lambda x: x['createdTime'], reverse=True)
     extract_data_tables = {n: t for n, t in config.EXTRACT_DATA_TABLES.items() if t['type'] == "google_sheet"}
     for spreadsheet in spreadsheets:
@@ -31,11 +46,7 @@ def main(log, only_table_name=None):
     os.makedirs(config.EXTRACT_DATA_PATH, exist_ok=True)
     for table_name, spreadsheet in matching_tables.items():
         if only_table_name is None or only_table_name == table_name:
-            if extract_data_tables[table_name].get('tab_name'):
-                sheet = gc.open(spreadsheet['name']).worksheet(extract_data_tables[table_name]['tab_name'])
-            else:
-                sheet = gc.open(spreadsheet['name']).sheet1
-            data = sheet.get_all_values()
+            data = get_all_sheet_values(gc, spreadsheet, extract_data_tables[table_name].get('tab_name'))
             with open(os.path.join(config.EXTRACT_DATA_PATH, f'{table_name}.csv'), 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerows(data)
