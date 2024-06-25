fc05580791dc7c8064f1d0a67e90c4477da4c8dc
ori <ori@uumpa.com>
2024-06-25 11:11:35 +0300

    update_smoove: ignore exceptions if recently updated (last 2 days)

--- vhskeelz_db/processing_record.py
+++ vhskeelz_db/processing_record.py
@@ -8,6 +8,10 @@ from . import config
 from .db import get_db_engine, conn_transaction_sql_handler
 
 
+class ProcessingRecordLogCommandSetStatusSuccess:
+    pass
+
+
 def start(process_name, process_id):
     with get_db_engine().connect() as conn:
         with conn.begin():
@@ -16,7 +20,8 @@ def start(process_name, process_id):
                     process_id varchar(255),
                     process_name varchar(255),
                     started_at timestamp not null default now(),
-                    finished_at timestamp
+                    finished_at timestamp,
+                    status varchar(255)
                 );
                 create table if not exists processing_record_log (
                     process_id varchar(255),
@@ -29,11 +34,16 @@ def start(process_name, process_id):
 
 
 def log(sql_execute, process_name, process_id, log_):
-    log_ = log_.replace("'", "''").replace('%', '%%')
-    now = datetime.datetime.now().isoformat()
-    sql_execute(dedent(f'''
-        insert into processing_record_log (process_id, process_name, log, log_at) values ('{process_id}', '{process_name}', '{log_}', '{now}');
-    '''))
+    if isinstance(log_, ProcessingRecordLogCommandSetStatusSuccess):
+        sql_execute(dedent(f'''
+            update processing_record set status = 'success' where process_id = '{process_id}' and process_name = '{process_name}';        
+        '''), force_commit=True)
+    else:
+        log_ = log_.replace("'", "''").replace('%', '%%')
+        now = datetime.datetime.now().isoformat()
+        sql_execute(dedent(f'''
+            insert into processing_record_log (process_id, process_name, log, log_at) values ('{process_id}', '{process_name}', '{log_}', '{now}');
+        '''))
 
 
 def finish(process_name, process_id):
@@ -42,6 +52,22 @@ def finish(process_name, process_id):
             conn.execute(dedent('''
                 update processing_record set finished_at = now() where process_id = %s and process_name = %s;
             '''), (process_id, process_name))
+
+
+def ignore_exceptions_if_recent_success(process_name):
+    if config.PROCESSING_RECORD_CONTEXT_IGNORE_EXCEPTIONS_IF_RECENT_SUCCESS:
+        with get_db_engine().connect() as conn:
+            with conn.begin():
+                row = conn.execute(dedent(f'''
+                    select 1 from processing_record
+                    where process_name = '{process_name}' and status = 'success' and finished_at >= now() - interval '2 day'
+                ''')).first()
+                if row:
+                    return True
+                else:
+                    return False
+    else:
+        return False
 
 
 @contextmanager
@@ -59,7 +85,10 @@ def processing_record(delayed_start=False):
                         yield log_partial
                 except:
                     log_partial(traceback.format_exc())
-                    raise Exception()
+                    if not ignore_exceptions_if_recent_success(config.PROCESSING_RECORD_NAME):
+                        raise Exception()
+                else:
+                    log_partial(ProcessingRecordLogCommandSetStatusSuccess())
                 finally:
                     finish(config.PROCESSING_RECORD_NAME, config.PROCESSING_RECORD_ID)
     elif delayed_start:
